1765effbce21366fb344be4c1f6805779032b469
Toriqul Rupai <toriqul@navigatus.com.au>
2026-07-21 08:43:08 +0600

    feat: Add environment variable for ADMIN_ACCESS_KEY in Cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
+  env: {
+    ADMIN_ACCESS_KEY: process.env.ADMIN_ACCESS_KEY || "",
+  },
 
   e2e: {
     baseUrl: "http://localhost:3001",
